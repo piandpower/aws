3978103c00ddcc8eea6dcbb9e78ac25dae3adcec
Ray Haleblian <ray@haleblian.com>
2014-11-22 21:44:26 -0800

    No more CloudSQL.

--- main.php
+++ main.php
@@ -21,14 +21,13 @@ function echo_row($row) {
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Media</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <link rel="stylesheet" href="http://ray.haleblian.com/css/normalize.css">
-        <link rel="stylesheet" href="http://ray.haleblian.com/css/main.css">
+        <link rel="stylesheet" href="css/404.css">
         <script src="http://ray.haleblian.com/js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
 <body>
@@ -44,7 +43,7 @@ $ini_path = "media.ini";
 $ini_array = parse_ini_file($ini_path);
 $conn = mysql_connect($ini_array["host"],
                       $ini_array["user"],
-                      '');
+                      $ini_array["passwd"]);
 $result = mysql_select_db($ini_array["db"]);
 
 if ($disc_id == "") {
@@ -62,35 +61,39 @@ if ($disc_id == "") {
 	}
 	$columns=1;
 	$i=0;
-    echo "<p>";
+    echo '<div class="container">';
+    echo '<h1>Discs</h1>';
 	for ($column=1; $column<=$columns; $column++) {
 		for (; $i<count($rows)*($column/$columns); $i++) {
 			echo_row($rows[$i]);
     		echo "</br>\n";
 		}
 	}
-    echo "</p>";
+    echo "</div>";
 
 } else {
 	# Disc contents.
 
-	$sql  = "select file.* from disc";
+	$sql  = "select name from disc";
+	$sql .= " where disc.id = '" . $disc_id . "'";
+	$result = mysql_query($sql);
+    $row = mysql_fetch_assoc($result);
+    echo '<h1>', $row['name'], '</h1>';
+
+	$sql  = "select disc.name,file.* from disc";
 	$sql .= " inner join file on disc.id=file.disc_id";
 	$sql .= " where disc.id = '" . $disc_id . "'";
 	$result = mysql_query($sql);
 	$previous_dir = "";
 	while ($row = mysql_fetch_assoc($result)) {
 		if ($previous_dir != $row['dir']) {
-		    echo "</p>";
-			echo "<p><strong>", $row['dir'], "</strong></br>";
+			echo "<h4>", $row['dir'], "</h4>";
     		$previous_dir = $row['dir'];
 		}
-		echo $row['name'], "</br>";
+		echo $row['name'], '</br>';
 	}
-	echo "</p>\n";
 }
 ?>
-
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="http://ray.haleblian.com/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
 <script src="http://ray.haleblian.com/js/plugins.js"></script>
